63cea52f62b3e5d6e6024e0ed95537121d8c42d2
ayedorao <ayelensamirad@gmail.com>
2026-01-14 14:07:11 -0300

    Updated firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyDUbdZOB9kyrfXHIoKIa5e462kBbVp1i7Q",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "saaszapa.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "saaszapa",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "saaszapa.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "729060474472",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:729060474472:web:f165d6e7917a408f8505dd",
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-3L"
 };
 
 const app = initializeApp(firebaseConfig);
